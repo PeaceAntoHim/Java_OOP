6e0022beb1cd50aea67abbd424eab75e79acdd71
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-04-29 19:13:55 +0700

    learn make package

---
+++ src/programmer/zaman/now/data/Product.java
@@ -0,0 +1,12 @@
+package programmer.zaman.now.data;
+
+public class Product {
+
+    String name;
+    int price;
+
+    Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+}
