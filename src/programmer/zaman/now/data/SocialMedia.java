2d31cd15aead993f62dbfb6389660cc001f301ee
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-04-30 17:17:56 +0700

    learn final method

--- src/programmer/zaman/now/data/SocialMedia.java
+++ src/programmer/zaman/now/data/SocialMedia.java
@@ -4,8 +4,18 @@ class SocialMedia {
     String name;
 }
 
+class Twitter extends SocialMedia {
+    // Leawrn Final mehthod
+    final void login(String username, String password) {
+
+    }
+}
+
 
 // Keyword final it't the last class
-final class Facebook extends SocialMedia {
-
+final class Facebook extends Twitter {
+    // This will error
+//    void login(String username, String password) {
+//        System.out.println("Test");
+//    }
 }
